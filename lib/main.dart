73fff66e6e6febc114ff8196bdf7cec6dffe17ac
Andrew Dias <andrew.1995@outlook.com>
2021-01-17 17:41:39 -0300

    wip(main): insert page ListTransferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Transferencias'),
       ),
       body: Column(
         children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on, ),
-              title: Text('100,0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on, ),
-              title: Text('100,0'),
-              subtitle: Text('1000'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on, ),
-              title: Text('100,0'),
-              subtitle: Text('1000'),
-            ),
-          ),
+          ItemTransferencia(Transferencia(numeroConta: 100, valor: 200.5)),
+          ItemTransferencia(Transferencia(numeroConta: 200, valor: 250.5)),
+          ItemTransferencia(Transferencia(numeroConta: 300, valor: 300.5)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-    ),
-    debugShowCheckedModeBanner: false,
-  ));
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          Icons.monetization_on,
+        ),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia({this.numeroConta, this.valor});
 }
